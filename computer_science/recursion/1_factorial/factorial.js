45f438e34dfcde044e674a92df0a2837b4831031
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2025-12-24 14:43:52 +0100

    Complete factorial exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  // If n is not a number not needed (isNaN(n)), or n is less than 0 or n is not a number
+  if (n < 0 || !Number.isInteger(n)) {
+    return
+  } else if (n === 1 || n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
 };
 
 // Do not edit below this line
