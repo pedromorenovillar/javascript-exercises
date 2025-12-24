13f83936690d1a4cc943debcd7229e9b7163b4ab
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2025-12-24 16:49:49 +0100

    Complete totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,40 @@
-const totalIntegers = function() {
-  
+const isObject = (value) => typeof value === 'object' && value !== null;
+
+const totalIntegers = function (obj) {
+  let count = 0
+  if (!isObject(obj)) {
+    return
+  }
+
+  const elements = Object.values(obj)
+
+  for (const el of elements) {
+    if (Number.isInteger(el)) {
+      count++
+    } else if (isObject(el)) {
+      count += totalIntegers(el)
+    }
+  }
+  return count
+  if (
+    Array.isArray(arrayOrObject) ||
+    (typeof arrayOrObject === "object" &&
+      arrayOrObject !== null &&
+      !Array.isArray(arrayOrObject))
+  ) {
+    const values = Object.values(arrayOrObject);
+    if (values.length === 1) {
+      if (Number.isInteger(values[0])) {
+        totalIntegers++
+      } else {
+        totalIntegers(values)
+      }
+    }
+  } else {
+    return;
+  }
+  return totalIntegers
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
